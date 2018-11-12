00661b866f09577054b8913e86fb589d61229dd6
beepbooprobot <benlangford10@gmail.com>
2018-11-12 21:58:01 +0000

    Added notes

--- Projectile Motion/PM4324774.m
+++ Projectile Motion/PM4324774.m
@@ -6,3 +6,11 @@
 % Add them together to get a resultant vector
 %Plot the motion along a 3d graph
 % 
+
+%% 3d plot
+
+% when you do a 3d plot, ensure that A) hold is on and B) view(3) is in
+% there
+%
+%
+%
